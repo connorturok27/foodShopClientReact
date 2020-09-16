b45ceffe71798b313d48929ece07a548cbff7d05
Christian Martinez <Christian.Martinez@udr.com>
2020-09-16 15:36:11 -0600

    Set initial project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ProductCard from './ProductCard/ProductCard'
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Food</h1>
+            <ProductCard/>
+        </div>
+    );
+}
+
+export default App;
