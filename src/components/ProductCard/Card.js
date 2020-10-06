e56ceece0d90c43c507e864a78ee318cf65d2cd8
Christian Martinez <Christian.Martinez@udr.com>
2020-10-05 21:01:12 -0600

    clientreact-31: - Search results added - Initial state is all the products

--- src/components/ProductCard/Card.js
+++ src/components/ProductCard/Card.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React, {useEffect, useState} from 'react';
+import {fade, makeStyles} from '@material-ui/core/styles';
 import {
     Card, CardActionArea
     , CardContent, Typography, CardMedia, Grid
 } from '@material-ui/core';
-import {useQuery} from 'react-query'
 import axios from 'axios'
 import {getFoodList} from '../../routes/shop.routes'
 import CollapseCardActions from "./CollapseCardActions";
+import TextField from "@material-ui/core/TextField";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -35,22 +35,83 @@ const useStyles = makeStyles((theme) => ({
     expandOpen: {
         transform: 'rotate(180deg)',
     },
+
+    searchRow: {
+      paddingTop: '1rem',
+      paddingBottom: '1rem'
+    },
+
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '75%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+
 }));
 
 export default function FoodCard() {
     const classes = useStyles();
 
-    const {isLoading, error, data} = useQuery("foodList", () =>
-        axios(getFoodList).then(res => res.data)
-    );
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const handleChange = event => {
+        if(event.target.value.split('').length > 2) {
+            setSearchTerm(event.target.value);
+        } else {
+            setSearchTerm('');
+        }
+    };
 
-    if (isLoading) return "Loading...";
-
-    if (error) return "An error has occurred: " + error.message;
+    useEffect(() => {
+        axios(`${getFoodList}?search=${searchTerm}`)
+            .then(res => setSearchResults(res.data))
+    }, [searchTerm]);
 
     return (
         <>
-            {data.map((food) => {
+            <Grid xs={12} className={classes.searchRow}>
+                <div className={classes.search}>
+                    <TextField id="search"
+                               label="Buscar..."
+                               variant="outlined"
+                               onChange={handleChange}
+                    />
+                </div>
+            </Grid>
+            {searchResults.map((food) => {
                 return (
                     <Grid key={food.id} item xs={12} sm={5} md={4} lg={3}>
                         <Card className={classes.root}>
